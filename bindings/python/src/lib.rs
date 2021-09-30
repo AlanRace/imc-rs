6b30a7f4325c9d8614958d845d10ab4b6a9c5ab4
Alan Race <alan.race@uni-marburg.de>
2021-09-30 17:15:41 +0200

    Added functions to get ids

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,5 +1,5 @@
-use pyo3::prelude::*;
 use pyo3::exceptions;
+use pyo3::prelude::*;
 
 use std::fs::File;
 
@@ -14,15 +14,12 @@ impl MCD {
     pub fn parse(filename: &str) -> PyResult<Self> {
         let file = match File::open(filename) {
             Ok(file) => file,
-            Err(error) => return Err(PyErr::new::<exceptions::PyIOError, _>(error))
+            Err(error) => return Err(PyErr::new::<exceptions::PyIOError, _>(error)),
         };
-        
 
         let mcd = imc_rs::MCD::parse(file, filename);
 
-        Ok(MCD {
-            mcd: mcd
-        })
+        Ok(MCD { mcd: mcd })
     }
 
     pub fn num_slides(&self) -> PyResult<usize> {
@@ -39,6 +36,22 @@ impl MCD {
         for slide in self.mcd.get_slides() {
             ids.append(&mut slide.get_panorama_ids());
         }
+
+        ids.sort();
+
+        Ok(ids)
+    }
+
+    pub fn acquisition_ids(&self) -> PyResult<Vec<u16>> {
+        let mut ids = Vec::new();
+
+        for slide in self.mcd.get_slides() {
+            for panorama in slide.get_panoramas() {
+                ids.append(&mut panorama.get_acquisition_ids());
+            }
+        }
+
+        ids.sort();
 
         Ok(ids)
     }
