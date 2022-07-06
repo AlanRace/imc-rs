fe671c4a983e33af26d028df43a93377dc8eacec
Alan Race <alan.race@uni-marburg.de>
2022-07-06 17:28:18 +0200

    Updated build and deploy scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-docker run --rm -v $(pwd):/io konstin2/maturin build --release -m /io/bindings/python/Cargo.toml
+docker build \
+    --build-arg http_proxy="http://www-proxy1.uni-marburg.de:3128" --build-arg https_proxy="http://www-proxy1.uni-marburg.de:3128" \
+    -t maturin . 
+
+docker run \
+    --env HTTP_PROXY="http://www-proxy1.uni-marburg.de:3128" --env HTTPS_PROXY="http://www-proxy1.uni-marburg.de:3128" \
+    --rm -v $(pwd):/io maturin build --release -m /io/bindings/python/Cargo.toml
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 # docker run --rm -v $(pwd):/io konstin2/maturin publish -m /io/bindings/python/Cargo.toml -r https://test.pypi.org/simple/
 
-twine upload --repository testpypi target/wheels/*.tar.gz
-twine upload --repository testpypi target/wheels/*manylinux*
+#twine upload --repository testpypi target/wheels/*.tar.gz
+#twine upload --repository testpypi target/wheels/*manylinux*
+
+twine upload target/wheels/*
